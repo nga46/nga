3cd4a0c741630a3fa1fe7cdac1ec8005da8398c3
nga46 <ngadinh0406@gmail.com>
2016-10-30 23:40:57 +0700

    oop1 lan 2

--- oop1/oop1.java
+++ oop1/oop1.java
@@ -5,7 +5,19 @@ package oop1;
  */
 public class oop1 {
     public static void main(String[] args) {
-        person student= new person();
+        person student= new person("nga", 21, 56);
+        System.out.println("name "+ student.name);
+        System.out.println("age "  +student.age);
+        System.out.println("weight " +student.weight);
+
+        System.out.println("---------------------------------------");
+
+
+        System.out.println(student);
+
+        System.out.println("---------------------------------------");
+
+
         student.setName("nga");
         student.setAge(21);
         student.setWeight(56);
@@ -20,6 +32,19 @@ class person {
     public String name;
     public int age;
     public int weight;
+
+    public person ( String name, int age, int weight)
+    {
+        this.name=name;
+        this.age=age;
+        this.weight=weight;
+    }
+
+    public String toString ()
+    {
+        return name + ": "+ age+": "  + weight;
+    }
+
 
     public void setName (String Name1)
     { this.name=Name1;}
